1722a768af05c91e87672cc361461862c5f6ef26
D_Zucho <danny.nico.dev@outlook.com>
2021-02-07 04:06:04 -0800

    create noteDOM

--- main.js
+++ main.js
@@ -13,3 +13,41 @@ let notes = [
     body: 'With some body section',
   },
 ];
+
+let filters = {
+  text: '',
+};
+
+let notesJSON = localStorage.getItem('notes');
+
+function renderNotesDOM(notes, filters) {
+  let filteredNotes = notes.filter(function (note) {
+    return note.title.toLowerCase().includes(filters.text.toLowerCase());
+  });
+
+  function displayNote(filteredNotes) {
+    // create note objects and add to DOM
+    filteredNotes.forEach((note) => {
+      let noteTitle = document.createElement('span');
+      noteTitle.textContent = '  - ' + note.title;
+      noteTitle.setAttribute('class', 'noteSpan');
+
+      let editButton = document.createElement('button');
+      editButton.textContent = ' Edit ';
+      editButton.setAttribute('class', 'editButton');
+
+      let deleteButton = document.createElement('button');
+      deleteButton.textContent = ' x ';
+      editButton.setAttribute('class', 'deleteButton');
+
+      let noteContainer = document.createElement('div');
+      noteContainer.setAttribute('class', 'noteComponent');
+
+      noteContainer.appendChild(noteTitle);
+      noteContainer.appendChild(editButton);
+      noteContainer.appendChild(deleteButton);
+
+      document.querySelector('.notes').appendChild(noteContainer);
+    });
+  }
+}
