30463926966c2f3cb96df22fb33fb0f41f9dd3a3
D_Zucho <danny.nico.dev@outlook.com>
2021-02-07 01:11:12 -0800

    add placeholder notes in JS

---
+++ main.js
@@ -0,0 +1,15 @@
+let notes = [
+  {
+    title: 'my next trip',
+    body: 'I would like to go to spain',
+  },
+  {
+    title: 'I think I am really lonely and depressed',
+    body:
+      "I think it's because I feel like I am a failure and dont know what to do with my life.",
+  },
+  {
+    title: 'And another note',
+    body: 'With some body section',
+  },
+];
